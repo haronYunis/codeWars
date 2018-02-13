18ee233d20bf1e2ed0a7f7ec5266107037762dc7
Haron Yunis <30669174+haronYunis@users.noreply.github.com>
2018-02-12 18:45:28 -0800

    Delete abbrevate-two-words.js

--- abbrevate-two-words.js
+++
@@ -1,9 +0,0 @@
-function abbrevName(name) {
-
-  for (var i = 0; name[i] !== '' ; i++);
-  var first = name[0];
-  var second = name[i + 1];
-
-  return first.toUpperCase() + '.' + second.toUpperCase();
-
-}

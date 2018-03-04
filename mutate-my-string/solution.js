e756253e9f3670b3a28700eea8c48ea863a0c708
haronYunis <haronyunis@icloud.com>
2018-03-04 12:40:03 -0800

    kata complete

---
+++ mutate-my-string/solution.js
@@ -0,0 +1,11 @@
+function mutateMyStrings(stringOne, stringTwo){
+  let one = stringOne.split(''), two = stringTwo.split('');
+  let endString = stringOne.concat('\n');
+  for (let i = 0; i < one.length; i++) {
+    if (one[i] !== two[i]) {
+      one[i] = two[i];
+      endString = endString.concat(`${one.join('')}\n`);
+    }
+  }
+  return endString;
+}
